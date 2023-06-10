eb6324c78e146a0e2e2becac12212fe96d80fce4
Danger <unknown707888@gmail.com>
2023-06-10 13:55:50 +0300

    Правка стили

---
+++ script.js
@@ -0,0 +1,21 @@
+let formElement = document.querySelector('.popup__form')
+let nameInput = document.querySelector('.popup__input-name')
+let jobInput = document.querySelector('.popup__input-job')
+
+// Обработчик «отправки» формы, хотя пока
+// она никуда отправляться не будет
+function handleFormSubmit (evt) {
+    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+                                                // Так мы можем определить свою логику отправки.
+                                                // О том, как это делать, расскажем позже.
+
+    // Получите значение полей jobInput и nameInput из свойства value
+
+    // Выберите элементы, куда должны быть вставлены значения полей
+
+    // Вставьте новые значения с помощью textContent
+}
+
+// Прикрепляем обработчик к форме:
+// он будет следить за событием “submit” - «отправка»
+formElement.addEventListener('submit', handleFormSubmit);

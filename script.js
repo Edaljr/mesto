de03ea0ef49d98c614c1570161a23df885c22d44
Danger <unknown707888@gmail.com>
2023-06-12 00:24:24 +0300

    JS: Заполнение кнопки лайк

--- script.js
+++ script.js
@@ -1,70 +1,42 @@
+let formElement = document.querySelector(".popup__form");
+let nameInput = document.querySelector(".popup__input-name");
+let jobInput = document.querySelector(".popup__input-job");
+let popup = document.querySelector(".popup");
+let profileTitle = document.querySelector(".profile__title");
+let profileSubtitle = document.querySelector(".profile__subtitle");
 
-let formElement = document.querySelector('.popup__form');
-let nameInput = document.querySelector('.popup__input-name');
-let jobInput = document.querySelector('.popup__input-job');
-let popup = document.querySelector('.popup');
-let profileTitle = document.querySelector('.profile__title');
-let profileSubtitle = document.querySelector('.profile__subtitle');
-let clickLike = document.querySelector('.cards__like');
-let saveButton = document.querySelector('.popup-save-btn');
+let saveButton = document.querySelector(".popup-save-btn");
 
-function onPopupOpen () {
-  popup.classList.add('popup__opened');
+function onPopupOpen() {
+  popup.classList.add("popup__opened");
+  setTextValue();
 }
 
 function onPopupClose() {
-  popup.classList.remove('popup__opened');
+  popup.classList.remove("popup__opened");
 }
 
-
-
-function handleFormSubmit(evt){
+function handleFormSubmit(evt) {
   evt.preventDefault();
   profileTitle.textContent = nameInput.value;
   profileSubtitle.textContent = jobInput.value;
+  onPopupClose();
 }
 
 function setTextValue() {
-
   nameInput.value = profileTitle.textContent.trim();
-  jobInput.value= profileSubtitle.textContent.trim();
+  jobInput.value = profileSubtitle.textContent.trim();
+}
+
+function clickLikeActive() {
+  let clickLike = document.querySelector(".cards__like");
+  if (clickLike.classList.contains("cards__like-active")) {
+    clickLike.classList.toggle("cards__like-active");
+  } else {
+    clickLike.classList.add("cards__like-active");
+  }
 }
 
 
 
-saveButton.addEventListener('click',() => {
-  handleFormSubmit
-})
-
-
-// function clickLikeActive (){
-//   clickLike.classList.add('cards__like-active');
-
-// }
-// function setClickLikeDisable (){
-//   clickLike.classList.remove('cards__like-active');
-// }
-
-// clickLike.addEventListener('click', function () {
-// });
-
-formElement.addEventListener('submit', handleFormSubmit);
-
-
-
-
-
- // Эта строчка отменяет стандартную отправку формы.
-//                                                 // Так мы можем определить свою логику отправки.
-//                                                 // О том, как это делать, расскажем позже.
-
-//     // Получите значение полей jobInput и nameInput из свойства value
-
-//     // Выберите элементы, куда должны быть вставлены значения полей
-
-//     // Вставьте новые значения с помощью textContent
-// }
-
-// // Прикрепляем обработчик к форме:
-// // он будет следить за событием “submit” - «отправка»
-
+formElement.addEventListener("submit", handleFormSubmit);

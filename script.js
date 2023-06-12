c9d0c8a2ab3acc158a15218f766ac3335ed51251
Danger <unknown707888@gmail.com>
2023-06-12 17:56:37 +0300

    БЭМ: Файловая структура

--- script.js
+++ script.js
@@ -4,12 +4,11 @@ let jobInput = document.querySelector(".popup__input-job");
 let popup = document.querySelector(".popup");
 let profileTitle = document.querySelector(".profile__title");
 let profileSubtitle = document.querySelector(".profile__subtitle");
-let saveButton = document.querySelector(".popup-save-btn");
+let profileEditBtn = document.querySelector(".profile__edit-btn")
 
 function onPopupOpen() {
-  popup.classList.add("popup__opened");
-  setTextValue();
-}
+popup.classList.add("popup__opened");}
+setTextValue()
 
 function onPopupClose() {
   popup.classList.remove("popup__opened");
@@ -27,13 +26,59 @@ function setTextValue() {
   jobInput.value = profileSubtitle.textContent.trim();
 }
 
-function clickLikeActive() {
-  let clickLike = document.querySelector(".cards__like");
-  if (clickLike.classList.contains("cards__like-active")) {
-    clickLike.classList.toggle("cards__like-active");
-  } else {
-    clickLike.classList.add("cards__like-active");
-  }
-}
+
+
+
+// function clickLikeActive() {
+//   let clickLike = document.querySelector(".cards__like");
+//   if (clickLike.classList.contains("cards__like-active")) {
+//     clickLike.classList.toggle("cards__like-active");
+//   } else {
+//     clickLike.classList.add("cards__like-active");
+//   }
+// }
 
 formElement.addEventListener("submit", handleFormSubmit);
+
+
+
+// let formElement = document.querySelector(".popup__form");
+// let nameInput = document.querySelector(".popup__input-name");
+// let jobInput = document.querySelector(".popup__input-job");
+// let popup = document.querySelector(".popup");
+// let profileTitle = document.querySelector(".profile__title");
+// let profileSubtitle = document.querySelector(".profile__subtitle");
+// let profileEditBtn = document.querySelector(".profile__edit-btn")
+
+// function onPopupOpen() {
+//   popup.classList.add("popup__opened");
+//   setTextValue();
+// }
+
+// function onPopupClose() {
+//   popup.classList.remove("popup__opened");
+// }
+
+// function handleFormSubmit(evt) {
+//   evt.preventDefault();
+//   profileTitle.textContent = nameInput.value;
+//   profileSubtitle.textContent = jobInput.value;
+//   onPopupClose();
+// }
+
+// function setTextValue() {
+//   nameInput.value = profileTitle.textContent.trim();
+//   jobInput.value = profileSubtitle.textContent.trim();
+// }
+
+
+// // function clickLikeActive() {
+// //   let clickLike = document.querySelector(".cards__like");
+// //   if (clickLike.classList.contains("cards__like-active")) {
+// //     clickLike.classList.toggle("cards__like-active");
+// //   } else {
+// //     clickLike.classList.add("cards__like-active");
+// //   }
+// // }
+
+// formElement.addEventListener("submit", handleFormSubmit);

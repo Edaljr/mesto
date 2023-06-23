4af058698a58ee036820d960b51914626589e34c
Danger <unknown707888@gmail.com>
2023-06-23 14:16:19 +0300

    Итерация 1

--- script/script.js
+++ script/script.js
@@ -1,118 +1,61 @@
-const initialCards = [
-  {
-    name: "Архыз",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
-  },
-  {
-    name: "Челябинская область",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
-  },
-  {
-    name: "Иваново",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
-  },
-  {
-    name: "Камчатка",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
-  },
-  {
-    name: "Холмогорский район",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
-  },
-  {
-    name: "Байкал",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
-  },
-];
-let cardCount = initialCards.length;
-let editFormElement = document.querySelector("#editForm");
-let addFormElement = document.querySelector("#addForm");
-let nameInput = document.querySelector('input[name="name"]');
-let jobInput = document.querySelector('input[name="job"]');
-let titleInput = document.querySelector('input[name="title"]');
-let linkInput = document.querySelector('input[name="link"]');
-let profileTitle = document.querySelector(".profile__title");
-let profileSubtitle = document.querySelector(".profile__subtitle");
-let profileEditBtn = document.querySelector(".profile__edit-btn");
-let profileAddBtn = document.querySelector(".profile__add-btn");
-let popupAdd = document.querySelector(".popup_add");
-let popupEdit = document.querySelector(".popup_edit");
-let cardsContainer = document.querySelector(".cards__container");
-let card = document.querySelector("#card").content;
+const editFormElement = document.querySelector("#editForm");
+const addFormElement = document.querySelector("#addForm");
+const nameInput = document.querySelector('input[name="name"]');
+const jobInput = document.querySelector('input[name="job"]');
+const popupTitleInput = document.querySelector('input[name="title"]');
+const popupLinkInput = document.querySelector('input[name="link"]');
+const profileTitle = document.querySelector(".profile__title");
+const profileSubtitle = document.querySelector(".profile__subtitle");
+const profileEditBtn = document.querySelector(".profile__edit-btn");
+const profileAddBtn = document.querySelector(".profile__add-btn");
+const popupAdd = document.querySelector(".popup_add");
+const popupEdit = document.querySelector(".popup_edit");
+const cardsContainer = document.querySelector(".cards__container");
+const card = document.querySelector("#card").content;
+const previewPopup = document.querySelector(".popup_image-scale")
 
-//dobavleniya cards + prisaivanie id
-const addCard = (name, link, cardId) => {
+//Create Card
+const createCard = (name, imgLink) => {
   const cardElement = card.querySelector(".cards__item").cloneNode(true);
-  cardElement.querySelector(".cards__photo").src = link;
+  cardElement.querySelector(".cards__photo").src = imgLink;
   cardElement.querySelector(".cards__title").textContent = name;
-  cardElement.setAttribute("id", cardId);
-  cardsContainer.prepend(cardElement);
-  let newCard = document.getElementById(cardId);
-  newCard.addEventListener("click", (evt) => {
-    evt.preventDefault();
-    showImagePopup(evt.target.parentElement.id);
+  const likeButton = cardElement.querySelector(".cards__like");
+  const deleteButton = cardElement.querySelector(".cards__btn-remove");
+  const cardImage = cardElement.querySelector(".cards__photo");
+  deleteButton.addEventListener("click", () => deleteCard(cardElement))
+  cardImage.addEventListener("click", () => handlePreviewImage(imgLink, event))
+  likeButton.addEventListener("click",  (evt) => {
+  handleLikeIcon(evt)
   });
-
-  //popup image
-  const showImagePopup = (id) => {
-    const selectedCard = document.getElementById(id);
-    const photoElementSrc =
-      selectedCard.getElementsByClassName("cards__photo")[0].src;
-    const elementTitle =
-      selectedCard.querySelector(".cards__title").textContent;
-    const photoPopup = document.querySelector(".popup_image-scale");
-    photoPopup.getElementsByClassName("popup__image")[0].src = photoElementSrc;
-    const subtitlePopup = document.querySelector(".popup__subtitle");
-    subtitlePopup.textContent = elementTitle;
-    photoPopup.classList.add("popup_opened");
-    const closeBtn = photoPopup.getElementsByClassName("popup__close-btn")[0];
-    closeBtn.addEventListener("click", () => {
-      photoPopup.getElementsByClassName("popup__image")[0].src = "";
-      onPopupClose(photoPopup.getElementsByClassName("popup__image")[0]);
-    });
-  };
-
-  //remove cards
-  const cardsRemoveBtn = document.querySelector(".cards__btn-remove");
-  cardsRemoveBtn.addEventListener("click", (evt) => {
-    evt.stopPropagation();
-    cardElement.remove();
-    cardCount--;
-  });
-
-  //Dobavleniya cards , like
-  cardElement.querySelector(".cards__like").addEventListener("click", (evt) => {
-    evt.stopPropagation();
-    evt.target.classList.toggle("cards__like-active");
-  });
+  return cardElement;
 };
 
-//download cards ,delete button
-window.addEventListener("load", (event) => {
-  for (let card = 0; card < initialCards.length; card++) {
-    addCard(initialCards[card].name, initialCards[card].link, card);
-    let cardEl = document.getElementById(card);
-    const cardsRemoveBtn = document.querySelector(".cards__btn-remove");
-    cardsRemoveBtn.addEventListener("click", (evt) => {
-      cardEl.remove();
-      cardCount--;
-    });
-  }
-});
+//Obrabotchik knopki like
+const handleLikeIcon = (evt) => {
+  evt.target.classList.toggle("cards__like-active");
+};
 
-//popup open
-const onPopupOpen = (popupName) => {
-  let closeBtn = null;
+//Render Cartochek
+const renderCard = (card, wrap) => {
+  wrap.prepend(card);
+}
+
+//Ydalenie Cartochek
+const deleteCard = (card) => {
+  card.remove();
+}
+
+
+//Popup open
+const onPopupOpen = (popupName, imgLink) => {
   if (popupName === "add") {
     popupAdd.classList.add("popup_opened");
-    closeBtn = popupAdd.getElementsByClassName("popup__close-btn")[0];
+  } else if (popupName === "preview") {
+    previewPopup.classList.add("popup_opened");
+    previewPopup.querySelector(".popup__image").src = imgLink;
   } else {
     popupEdit.classList.add("popup_opened");
-    closeBtn = popupEdit.getElementsByClassName("popup__close-btn")[0];
   }
-  closeBtn.addEventListener("click", (evt) => {
-    onPopupClose();
-  });
 };
 
 //Popup close
@@ -123,45 +66,66 @@ function onPopupClose() {
   }
 }
 
-//submit form
-const handleFormSubmit = (evt) => {
+function setEditFormTextValue() {
+  nameInput.value = profileTitle.textContent.trim();
+  jobInput.value = profileSubtitle.textContent.trim();
+}
+
+function setCardsTextValue() {
+  popupTitleInput.value = popupTitleInput.textContent;
+  popupLinkInput.value = popupLinkInput.textContent;
+  }
+
+
+//download cards ,delete button
+window.addEventListener("load", (event) => {
+  for (let card = 0; card < initialCards.length; card++) {
+    const newCard = createCard(initialCards[card].name, initialCards[card].link);
+    renderCard(newCard, cardsContainer)
+  }
+  const closeBtn = document.querySelectorAll(".popup__close-btn");
+  closeBtn.forEach((button) => {
+    button.addEventListener("click", onPopupClose);
+  })
+});
+
+//Submit Form
+const handleEditFormSubmit = (evt) => {
   evt.preventDefault();
   profileTitle.textContent = nameInput.value;
   profileSubtitle.textContent = jobInput.value;
   onPopupClose();
 };
 
-function setTextValue() {
-  nameInput.value = profileTitle.textContent.trim();
-  jobInput.value = profileSubtitle.textContent.trim();
-}
-
-function setCardsTextValue() {
-  if ((titleInput.value = titleInput.textContent)) {
-    titleInput.placeholder = "Название";
-  }
-
-  if ((linkInput.value = linkInput.textContent)) {
-    linkInput.placeholder = "Ссылка на картинку";
-  }
-}
-
 //submit dobavleniya cards
 const handleAddFormSubmit = (evt) => {
   evt.preventDefault();
-  addCard(titleInput.value, linkInput.value, cardCount);
-  cardCount++;
+  const newCard = createCard(popupTitleInput.value, popupLinkInput.value);
+  renderCard(newCard, cardsContainer)
   onPopupClose();
 };
 
-profileEditBtn.addEventListener("click", function () {
+const handlePreviewImage = (data, evt) => {
+  evt.preventDefault();
+  onPopupOpen("preview", data)
+};
+
+profileEditBtn.addEventListener("click",  (evt) => {
+  evt.preventDefault();
   onPopupOpen();
-  setTextValue();
+  setEditFormTextValue();
 });
 
-editFormElement.addEventListener("submit", handleFormSubmit);
+editFormElement.addEventListener("submit", handleEditFormSubmit);
+
 addFormElement.addEventListener("submit", handleAddFormSubmit);
-profileAddBtn.addEventListener("click", function () {
+
+profileAddBtn.addEventListener("click",  (evt) => {
+  evt.preventDefault();
   onPopupOpen("add");
   setCardsTextValue();
 });
+
+
+
+

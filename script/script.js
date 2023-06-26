303d51be6f7a830de00408ef8d06b77d9fcada12
Danger <unknown707888@gmail.com>
2023-06-26 18:11:51 +0300

    Итерация 3

--- script/script.js
+++ script/script.js
@@ -12,7 +12,12 @@ const popupAdd = document.querySelector(".popup_add");
 const popupEdit = document.querySelector(".popup_edit");
 const cardsContainer = document.querySelector(".cards__container");
 const card = document.querySelector("#card").content;
-const previewPopup = document.querySelector(".popup_image-scale")
+const previewPopup = document.querySelector(".popup_image-scale");
+const imageElement = document.querySelector(".popup__image");
+const previewPopupSubtitle = document.querySelector(".popup__subtitle");
+const closeBtnPreview = previewPopup.querySelector(".popup__close-btn");
+const closeBtnAdd = popupAdd.querySelector(".popup__close-btn");
+const closeBtnEdit = popupEdit.querySelector(".popup__close-btn");
 
 //Create Card
 const createCard = (name, imgLink) => {
@@ -23,10 +28,10 @@ const createCard = (name, imgLink) => {
   const likeButton = cardElement.querySelector(".cards__like");
   const deleteButton = cardElement.querySelector(".cards__btn-remove");
   const cardImage = cardElement.querySelector(".cards__photo");
-  deleteButton.addEventListener("click", () => deleteCard(cardElement))
-  cardImage.addEventListener("click", () => handlePreviewImage(imgLink, event))
-  likeButton.addEventListener("click",  (evt) => {
-  handleLikeIcon(evt)
+  deleteButton.addEventListener("click", () => deleteCard(cardElement));
+  cardImage.addEventListener("click", () => handlePreviewImage(imgLink, name));
+  likeButton.addEventListener("click", (evt) => {
+    handleLikeIcon(evt);
   });
   return cardElement;
 };
@@ -39,24 +44,22 @@ const handleLikeIcon = (evt) => {
 //Render Cartochek
 const renderCard = (card, wrap) => {
   wrap.prepend(card);
-}
+};
 
 //Ydalenie Cartochek
 const deleteCard = (card) => {
   card.remove();
-}
+};
 
 //Popup open
 const onPopupOpen = (modalWindow) => {
-    modalWindow.classList.add("popup_opened");}
-
-  //   //
+  modalWindow.classList.add("popup_opened");
+};
 
 //Popup close
-function onPopupClose() {
-  const popupOpened = document.querySelector('.popup_opened')
-  popupOpened.classList.remove("popup_opened");
-  }
+const onPopupClose = (modalWindow) => {
+  modalWindow.classList.remove("popup_opened");
+};
 
 function setEditFormTextValue() {
   nameInput.value = profileTitle.textContent.trim();
@@ -66,20 +69,17 @@ function setEditFormTextValue() {
 function setCardsTextValue() {
   inputNameFormAddNewCard.value = inputNameFormAddNewCard.textContent;
   inputLinkFormAddNewCard.value = inputLinkFormAddNewCard.textContent;
-  }
-
+}
 
 //download cards ,delete button
 window.addEventListener("load", (event) => {
   for (let card = 0; card < initialCards.length; card++) {
-    const newCard = createCard(initialCards[card].name, initialCards[card].link);
-    renderCard(newCard, cardsContainer)
+    const newCard = createCard(
+      initialCards[card].name,
+      initialCards[card].link
+    );
+    renderCard(newCard, cardsContainer);
   }
-  const closeBtn = document.querySelectorAll(".popup__close-btn");
-  closeBtn.forEach((button) => {
-    button.addEventListener("click", () =>
-    onPopupClose());
-  })
 });
 
 //Submit Form
@@ -93,18 +93,22 @@ const handleEditFormSubmit = (evt) => {
 //submit dobavleniya cards
 const handleAddFormSubmit = (evt) => {
   evt.preventDefault();
-  const newCard = createCard(inputNameFormAddNewCard.value, inputLinkFormAddNewCard.value);
-  renderCard(newCard, cardsContainer)
+  const newCard = createCard(
+    inputNameFormAddNewCard.value,
+    inputLinkFormAddNewCard.value
+  );
+  renderCard(newCard, cardsContainer);
   onPopupClose(popupAdd);
 };
 
-const handlePreviewImage = (imgLink, evt) => {
-  evt.preventDefault();
-  previewPopup.querySelector(".popup__image").src = imgLink;
-  onPopupOpen(previewPopup)
+const handlePreviewImage = (imgLink, name) => {
+  imageElement.src = imgLink;
+  imageElement.alt = `Изображение ${name}`;
+  previewPopupSubtitle.textContent = name;
+  onPopupOpen(previewPopup);
 };
 
-profileEditBtn.addEventListener("click",  (evt) => {
+profileEditBtn.addEventListener("click", (evt) => {
   evt.preventDefault();
   onPopupOpen(popupEdit);
   setEditFormTextValue();
@@ -114,12 +118,18 @@ editFormElement.addEventListener("submit", handleEditFormSubmit);
 
 addFormElement.addEventListener("submit", handleAddFormSubmit);
 
-profileAddBtn.addEventListener("click",  (evt) => {
+profileAddBtn.addEventListener("click", (evt) => {
   evt.preventDefault();
   onPopupOpen(popupAdd);
   setCardsTextValue();
 });
 
+closeBtnPreview.addEventListener("click", () => onPopupClose(previewPopup));
+closeBtnAdd.addEventListener("click", () => onPopupClose(popupAdd));
+closeBtnEdit.addEventListener("click", () => onPopupClose(popupEdit));
 
-
-
+// Решением для крестика было глянуть самое первое твоё замечание, надеюсь правильно понял ))
+// Обработчик на крестик будет добавляться итеративно.
+// Открыли 5 раз один и тот же попап - будет добавлено 5 обработчиков,
+// это не верно. В проекте 3 попапа, достаточно слушатель закрытия попапа на крестик
+// добавить один раз на каждый попап в глобальном скоупе.
